ee528f0421bd480de1b6f1174aeb2942d00dde15
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-07-23 22:07:51 +0800

    Update if-statements.sh

--- learn-linux-tv/if/if-statements.sh
+++ learn-linux-tv/if/if-statements.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
-mynum=200
+mynum=300
 
 if [ $mynum -eq 200 ]
 then
     echo "The condition is true."
+else
+    echo "Not equal 200"
 fi
+
+pwd "~/if-statements.sh"
+
+if [ -f if-statements.sh ]
+then
+    echo "The file exists."
+else
+    echo "The file does not exists."
+fi
+
+# command=/usr/bin/htop
+# if [ -f $command ]
+
+command=htop
+if command -v $command
+then
+    echo "$command is available!"
+else
+    echo "$command is not available"
+    # sudo apt update && sudo apt install -y htop
+fi
+
+# $command
